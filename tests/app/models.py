bce807fe58577d96b9f61aa08c4f8e9109619433
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2021-10-08 09:18:17 +0300

    fix demo recipes pagination, show how to enable change view

--- tests/app/models.py
+++ tests/app/models.py
@@ -14,18 +14,21 @@ class ExternalUser(models.Model):
         verbose_name = "External User Model"
 
 
-# mixin DfesAdminModelMixin
+# 1. mixin DfesAdminModelMixin
 class ExternalUserAdmin(DfesAdminModelMixin, admin.ModelAdmin):
     list_display = (
         "id",
         "username",
     )
 
-    # and implement get_list method to return
+    # 2. and implement get_list method with returning
     # `{"total": <total number or items>, "items": <list of ExternalUser instances>}`
     def get_list(self, request, page_num, list_per_page):
 
-        # pull data from some service
+        # 2.1 pull data from some service, where
+        #   search = request.GET.get('q')
+        #   order_by = request.GET.get('o')
+        #   some_list_filter = request.GET.get('some_list_filter')
         data = {
             "total": 1,
             "users": [
@@ -33,6 +36,7 @@ class ExternalUserAdmin(DfesAdminModelMixin, admin.ModelAdmin):
             ],
         }
 
+        # 2.2 map data to model instances
         items = [ExternalUser(**i) for i in data.get("users") or []]
 
         return {
@@ -40,7 +44,12 @@ class ExternalUserAdmin(DfesAdminModelMixin, admin.ModelAdmin):
             "items": items,
         }
 
-    # other django admin customization
+    # 3. other standart django admin customization
+    def get_object(self, request, object_id, *args, **kwargs):
+        # 3.1 fetch object from external service
+        user = {"id": 1, "username": "User1"}
+        # 3.2 map data to model instance
+        return ExternalUser(**user)
 
 
 admin.site.register(ExternalUser, ExternalUserAdmin)
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -23,6 +23,19 @@ def get_model_changelist_url(model):
     )
 
 
+def get_model_change_url(model, pk):
+    opts = model._meta
+    return reverse(
+        "admin:%s_%s_change"
+        % (
+            opts.app_label,
+            opts.model_name,
+        ),
+        args=(pk,),
+        current_app=admin.AdminSite().name,
+    )
+
+
 @pytest.mark.django_db
 def test_changelist():
 
@@ -35,6 +48,7 @@ def test_changelist():
     c = Client()
     c.force_login(admin_user)
 
+    # changelist
     response = c.get(get_model_changelist_url(ExternalUser))
     assert response.status_code == 200
 
@@ -43,3 +57,11 @@ def test_changelist():
 
     # check paginator
     assert "1 {}".format(ExternalUser._meta.verbose_name) in content
+
+    # change
+    response = c.get(get_model_change_url(ExternalUser, pk=1))
+    assert response.status_code == 200
+
+    content = response.content.decode()
+    assert "Change {}".format(ExternalUser._meta.verbose_name) in content
+    assert "User1" in content
